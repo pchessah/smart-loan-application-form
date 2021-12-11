56789820586ca02713b8e55f524aa6beed1378d7
Paul Chesa <paulchesa1@gmail.com>
2021-12-11 11:38:15 +0300

    📦✨✨Pages and components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './pages/intro/intro.component';
+import { StepsComponent } from './pages/steps/steps.component';
+import { SummaryComponent } from './pages/summary/summary.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { StepOneComponent } from './components/step-one/step-one.component';
+import { StepTwoComponent } from './components/step-two/step-two.component';
+import { StepThreeComponent } from './components/step-three/step-three.component';
+import { StepFourComponent } from './components/step-four/step-four.component';
+import { StepSummaryComponent } from './components/step-summary/step-summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    StepsComponent,
+    SummaryComponent,
+    NavbarComponent,
+    FooterComponent,
+    StepOneComponent,
+    StepTwoComponent,
+    StepThreeComponent,
+    StepFourComponent,
+    StepSummaryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/step-summary/step-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-step-summary',
+  templateUrl: './step-summary.component.html',
+  styleUrls: ['./step-summary.component.scss']
+})
+export class StepSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
