8be243f2ca75fb42c1b7b7cd9906f63ca0d10205
Paul Chesa <paulchesa1@gmail.com>
2021-12-14 20:41:32 +0300

    :sparkles: step two form finished

--- src/app/components/step-two/step-two.component.ts
+++ src/app/components/step-two/step-two.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { IApplicantDetails } from 'src/app/interfaces/IApplicantDetails';
+import { CurrentApplicantStateService } from 'src/app/services/current-applicant-state.service';
 
 @Component({
   selector: 'app-step-two',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StepTwoComponent implements OnInit {
 
-  constructor() { }
+  stepTwoFormGroup: FormGroup;
 
-  ngOnInit(): void {
+  tempApplicantDetails: IApplicantDetails;
+
+  constructor(private _fb: FormBuilder,
+              private _currentApplicantStateService: CurrentApplicantStateService)
+  {
+    this.stepTwoFormGroup =
+      this._fb.group({
+        loanRepaymentDate: ['', [Validators.required, Validators.max(31), Validators.min(1)]],
+        loanTerm: ['', Validators.required],
+      });
+  }
+
+
+  ngOnInit()
+  { 
+    this._currentApplicantStateService.currentApplicantDetails$.subscribe((c) => {
+      this.tempApplicantDetails = c;
+    })
+  }
+
+  getErrorMessage()
+  {
+    const formField = this.stepTwoFormGroup.get('loanRepaymentDate');
+  
+    if (formField?.hasError('required')) {
+      return 'You must enter a value';
+    } else if (formField?.hasError('max')) {
+      return 'Maximum value is 31';
+    } else if (formField?.hasError('min')) {
+      return 'Minimum value is 1';
+    } else {
+      return '';
+    }
+  }
+
+  onSubmitRepaymentDetails()
+  {
+    this.tempApplicantDetails.loanRepaymentDate = this.stepTwoFormGroup.get('loanRepaymentDate')?.value;
+    this.tempApplicantDetails.loanTerm = this.stepTwoFormGroup.get('loanTerm')?.value;
+    this._currentApplicantStateService.setCurrentApplicantDetails(this.tempApplicantDetails);
   }
 
 }
