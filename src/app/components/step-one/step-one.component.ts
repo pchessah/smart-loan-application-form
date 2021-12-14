847ecd6da539a70532419ddc1c477dca5480145f
Paul Chesa <paulchesa1@gmail.com>
2021-12-14 20:43:19 +0300

    :sparkles: step one

--- src/app/components/step-one/step-one.component.ts
+++ src/app/components/step-one/step-one.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IApplicantDetails } from 'src/app/interfaces/IApplicantDetails';
+import { CurrentApplicantStateService } from 'src/app/services/current-applicant-state.service';
 
 @Component({
   selector: 'app-step-one',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StepOneComponent implements OnInit {
 
-  constructor() { }
+  stepOneFormGroup: FormGroup;
+
+  tempApplicantDetails: IApplicantDetails;
+
+  constructor(private _fb: FormBuilder,
+    private _currentApplicantStateService: CurrentApplicantStateService) {
+    this.stepOneFormGroup =
+      this._fb.group({
+        loanAmount: ['', Validators.required],
+        loanPurpose: ['', Validators.required],
+      });
+  }
+
 
   ngOnInit(): void {
+    this._currentApplicantStateService.currentApplicantDetails$.subscribe((c) => {
+      this.tempApplicantDetails = c;
+    })
   }
+
+  onSubmitLoanDetails()
+  {
+      this.tempApplicantDetails.loanAmount = this.stepOneFormGroup.get('loanAmount')?.value ;
+      this.tempApplicantDetails.loanPurpose = this.stepOneFormGroup.get('loanPurpose')?.value;
+      this._currentApplicantStateService.setCurrentApplicantDetails(this.tempApplicantDetails);
+  }
+
+
 
 }
