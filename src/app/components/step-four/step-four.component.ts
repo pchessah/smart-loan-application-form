e3f78bb7d330cc45eee9f72d2bf8bd9aeca72668
Paul Chesa <paulchesa1@gmail.com>
2021-12-14 20:42:53 +0300

    :sparkles: step four done

--- src/app/components/step-four/step-four.component.ts
+++ src/app/components/step-four/step-four.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { IApplicantDetails } from 'src/app/interfaces/IApplicantDetails';
+import { CurrentApplicantStateService } from 'src/app/services/current-applicant-state.service';
 
 @Component({
   selector: 'app-step-four',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./step-four.component.scss']
 })
 export class StepFourComponent implements OnInit {
+  
+  stepFourFormGroup: FormGroup;
 
-  constructor() { }
+  tempApplicantDetails: IApplicantDetails;
 
-  ngOnInit(): void {
+  constructor(private _fb: FormBuilder,
+              private _currentApplicantStateService: CurrentApplicantStateService)
+  {
+    this.stepFourFormGroup =
+    this._fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+    });
+   }
+
+  ngOnInit()
+  {
+    this._currentApplicantStateService.currentApplicantDetails$.subscribe((c) => {
+      this.tempApplicantDetails = c;
+    })
+  }
+
+  onSubmitPersonalDetails() 
+  {
+    this.tempApplicantDetails.firstName = this.stepFourFormGroup.get('firstName')?.value;
+    this.tempApplicantDetails.lastName = this.stepFourFormGroup.get('lastName')?.value;
+    this.tempApplicantDetails.email = this.stepFourFormGroup.get('email')?.value;
+    this.tempApplicantDetails.phone = this.stepFourFormGroup.get('phone')?.value;
+
+    this._currentApplicantStateService.setCurrentApplicantDetails(this.tempApplicantDetails);
   }
 
 }
