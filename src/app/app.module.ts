8a60609ef9b284829ce6ac90bd66078c39d8a5db
Paul Chesa <paulchesa1@gmail.com>
2021-12-14 20:40:55 +0300

    :sparkles: new components, new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,37 +11,39 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatStepperModule} from '@angular/material/stepper';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { IntroComponent } from './pages/intro/intro.component';
 import { StepsComponent } from './pages/steps/steps.component';
-import { SummaryComponent } from './pages/summary/summary.component';
+
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { StepOneComponent } from './components/step-one/step-one.component';
 import { StepTwoComponent } from './components/step-two/step-two.component';
 import { StepThreeComponent } from './components/step-three/step-three.component';
 import { StepFourComponent } from './components/step-four/step-four.component';
 import { StepSummaryComponent } from './components/step-summary/step-summary.component';
-import { NextPrevBtnsComponent } from './components/next-prev-btns/next-prev-btns.component';
+import { SuccessComponent } from './pages/success/success.component';
 
-const MATERIAL_MODULES = [MatCardModule, MatButtonModule, MatStepperModule, MatFormFieldModule, MatInputModule ]
+const MATERIAL_MODULES = [MatCardModule, MatButtonModule, MatStepperModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatProgressSpinnerModule, MatSnackBarModule];
 @NgModule({
   declarations: [
     AppComponent,
     IntroComponent,
     StepsComponent,
-    SummaryComponent,
     NavbarComponent,
-    FooterComponent,
+
     StepOneComponent,
     StepTwoComponent,
     StepThreeComponent,
     StepFourComponent,
     StepSummaryComponent,
-    NextPrevBtnsComponent
+    SuccessComponent,
+
   ],
   imports: [
     BrowserModule,
